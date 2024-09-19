7da3fca16eef5965b6a03128f9597c24c120ee23
Fernando Hernandez <ferandree270904@gmail.com>
2024-09-18 18:02:51 -0600

    Login error Screen

---
+++ app/src/main/java/com/example/tutoriasuvg/login/login_error.kt
@@ -0,0 +1,168 @@
+package com.example.tutoriasuvg.login
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tutoriasuvg.R
+import com.example.tutoriasuvg.ui.theme.TutoriasUVGTheme
+
+
+@Composable
+fun LoginScreenError(){
+    var email by remember { mutableStateOf("")}
+    var password by remember { mutableStateOf("") }
+    var showError by remember { mutableStateOf(false) }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .background(MaterialTheme.colorScheme.background),
+            contentAlignment = Alignment.Center
+
+        ){
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.letras_uvg),
+                    contentDescription = "Logo en letras UVG",
+                    modifier = Modifier
+                        .size(150.dp)
+                        .padding(bottom = 24.dp)
+                )
+                Text(
+                    text = "Email",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp)
+                )
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it},
+                    label = { Text("Correo Institucional")},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    singleLine = true
+                )
+                Text(
+                    text = "Password",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp)
+                )
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it},
+                    label = { Text("Contraseña")},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    singleLine = true,
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                )
+                if (showError){
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                   ){
+                        Icon(
+                            imageVector = Icons.Default.Warning,
+                            contentDescription = "Error",
+                            tint = androidx.compose.ui.graphics.Color.Red,
+                            modifier = Modifier.size(20.dp)
+                        )
+                        Text(
+                            text = "Contraseña o Usuario Incorrecto",
+                            color = androidx.compose.ui.graphics.Color.Red,
+                            modifier = Modifier.padding(start = 8.dp)
+                        )
+                    }
+
+                }
+
+                Button(
+                    onClick = {
+                        if (email.isEmpty() || password.isEmpty()) {
+                            showError = true
+                        } else {
+                            showError = false
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(48.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
+                ){
+                    Text(text = "Sign In", fontSize = 16.sp)
+                }
+                TextButton(
+                    onClick = { /* Lógica para registrarse */ },
+                    modifier = Modifier.padding(top = 16.dp)
+                ) {
+                    Text(
+                        text = "¿Aún no tienes cuenta? Regístrate acá",
+                        style = MaterialTheme.typography.bodyMedium,
+                        textAlign = TextAlign.Center,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenErrorPreview(){
+    TutoriasUVGTheme {
+        LoginScreenError()
+    }
+}
