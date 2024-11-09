149ef223aeda4a5a90da7f1c1246fcbdafc0cc7d
Fernando Hernandez <ferandree270904@gmail.com>
2024-11-09 16:47:32 -0600

    Dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.jetbrains.kotlin.serialization)
     id("kotlin-kapt")
-    id("com.google.gms.google-services")
-    id("com.google.firebase.crashlytics")
+    id("com.google.gms.google-services") // Mantener este plugin
+    id("com.google.firebase.crashlytics") // Firebase Crashlytics para reportar errores
 }
 
 android {
@@ -59,35 +59,44 @@ android {
 }
 
 dependencies {
-
+    // AndroidX Core and Lifecycle
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Jetpack Compose and Navigation
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
-    implementation(libs.androidx.icons.extended)
-    implementation(libs.androidx.compose.navigation)
-    implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.icons.extended)
     implementation(libs.androidx.material3)
-    implementation(libs.coilCompose)
+    implementation(libs.androidx.compose.navigation)
+
+    // Serialization and DataStore
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.datastore.preferences)
+
+    // Splash Screen
     implementation(libs.androidx.splashscreen)
+
+    // Room Database
     implementation(libs.androidx.room.runtime)
-    implementation(libs.firebase.common.ktx)
-    implementation(libs.firebase.auth.ktx)
-    implementation(libs.firebase.firestore.ktx)
-    //implementation(libs.androidx.ui.desktop)
     kapt(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
+
+    // Coil for Image Loading
+    implementation(libs.coilCompose)
+
+    // Firebase dependencies (utilizando BoM)
     implementation(platform(libs.firebase.bom))
-
-    implementation(libs.firebase.crashlytics.ktx)
     implementation(libs.firebase.analytics.ktx)
+    implementation(libs.firebase.crashlytics.ktx)
+    implementation(libs.firebase.auth.ktx)
+    implementation(libs.firebase.firestore.ktx)
+    implementation(libs.firebase.common.ktx)
 
-
-
+    // Testing dependencies
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -95,5 +104,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Kotlin script runtime
     implementation(kotlin("script-runtime"))
 }
